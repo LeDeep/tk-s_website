b2aad502783152b36620be6e25604ecf96f30683
Deep <smarreddy1@gmail.com>
2013-07-16 13:19:12 -0700

    added contact page to staticpages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ TksWebsite::Application.routes.draw do
   root to: "static_pages#home"
 
   match '/about', to: "static_pages#about"
+  match '/contact', to: "static_pages#contact"
 
 
   # The priority is based upon order of creation:
