8c7fbe85b5c19a915266470842bb2baa9d88f347
Deep <smarreddy1@gmail.com>
2013-07-26 10:07:05 -0700

    updates to users and views profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,9 @@ class Profile < ActiveRecord::Base
   attr_accessible :bio, :favorite_family_memory, :location, :birthday, :user_id
 
   belongs_to :user
+
+  def age
+    Date.today.year - self.birthday.try(:year)
+  end
 end
+
