5ea02fb7d23dd312de4f38e9b1eaf0ad5f71c735
Deep <smarreddy1@gmail.com>
2013-08-22 10:53:56 -0700

    updates

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+    current_user = user
   end
 
   def signed_in?
@@ -10,7 +10,7 @@ module SessionsHelper
   end
 
   def current_user?(user)
-    @current_user = user
+    user == current_user
   end
 
   def signed_in_user
