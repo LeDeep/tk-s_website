682ab86898a4a253fe26d427cca5b4cf4c79f937
Deep <smarreddy1@gmail.com>
2013-08-22 10:01:39 -0700

    blogs controller and specs

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -9,3 +9,14 @@ describe BlogsController do
     it {should route(:get, '/users/new').to :action => :new}
     it {should route(:post, '/users').to :action => :create}
   end
+
+  context 'submitting to the create action' do 
+    before {post blogs_path}
+    specify {response.should redirect_to(signin_path)}
+  end
+
+  context 'submitting to the destroy action' do 
+    before { delete blog_path(FactoryGirl.create(:blog))}
+    specify { response.should redirect_to(signin_path)}
+  end
+end
